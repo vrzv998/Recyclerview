3f0464e3065c79fd94c0b4c290fbed180b89d0fd
vrzv998 <32388502+vrzv998@users.noreply.github.com>
2018-10-15 22:12:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/vrzv/recyclerview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.vrzv.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView r1;
+    String s1[],s2[];
+    int imageResource[] ={R.drawable.bayam,R.drawable.makaroni};
+
+    MyOwnAdapter ad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        r1 = (RecyclerView)findViewById(R.id.myRecycler);
+
+        s1 = getResources().getStringArray(R.array.food_name);
+        s2 = getResources().getStringArray(R.array.desc);
+
+        ad = new MyOwnAdapter(this,s1,s2,imageResource);
+
+        r1.setAdapter(ad);
+        r1.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
